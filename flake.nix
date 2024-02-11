6e0851c6d28cd0bdc273a37be05815cd8e398bd7
Victor Rodriguez <awtgerry@gmail.com>
2024-02-10 20:02:21 -0600

    try naersk

--- flake.nix
+++ flake.nix
@@ -1,32 +1,50 @@
 {
-  description = "Rust dependency management";
-
   inputs = {
-    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs, ... } @inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      devShells.${system}.default = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [
-          dbus
-          gdk-pixbuf
-          libnotify
-          pkg-config
-          xorg.libX11
+  outputs = { self, flake-utils, naersk, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
 
-          rust-analyzer
-          rustup
-          rustc
-          cargo
-          gcc
-          glib
-          glibc
-        ];
-      };
-    };
+        naersk' = pkgs.callPackage naersk {};
+
+      in rec {
+        # For `nix build` & `nix run`:
+        defaultPackage = naersk'.buildPackage {
+          nativeBuildInputs = with pkgs; [
+            dbus
+            gdk-pixbuf
+            libnotify
+            pkg-config
+            xorg.libX11
+          ];
+          src = ./.;
+        };
+
+        # For `nix develop`:
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            dbus
+            gdk-pixbuf
+            libnotify
+            pkg-config
+            xorg.libX11
+
+            rust-analyzer
+            rustup
+            rustc
+            cargo
+            gcc
+            glib
+            glibc
+          ];
+        };
+      }
+    );
 }
